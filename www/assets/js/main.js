228753cce92cdfccc32389a4f62033e23c7fdeab
davidmalcat <david>
2023-02-15 19:53:41 +0100

    homepage styles

--- www/assets/js/main.js
+++ www/assets/js/main.js
@@ -1,3 +1,15 @@
 $(function() {
 	replaceSVG();
+
+	var scroll = new SmoothScroll('a[href*="#"]');
+
+	$('#slider').slick({
+		dots: true,
+		infinite: true,
+		speed: 500,
+		vertical: true,
+		swipe: false, 
+		verticalSwiping: true,
+
+	});
 });
